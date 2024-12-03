a87901fd217832c0be399c2405d1b4f11203e988
bebor <bordax@proton.me>
2024-12-03 13:35:33 +0100

    packaging

---
+++ rediskit/__init__.py
@@ -0,0 +1,19 @@
+"""
+RedisKit - An async Redis client toolkit
+"""
+
+from .client import AsyncClient, AsyncStorer, AsyncMessageBroker
+from .config import RedisConfig
+from .exceptions import RedisKitError, ConnectionError, SerializationError
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "AsyncClient",
+    "AsyncStorer",
+    "AsyncMessageBroker",
+    "RedisConfig",
+    "RedisKitError",
+    "ConnectionError",
+    "SerializationError",
+]
